f6b42c77f3ffe2e962a30dc07f72b4e37050275b
you.wenbo <you.wenbo@immomo.com>
2021-08-20 15:17:05 +0800

    增加：添加测试 ButterKnife 编译失败的环境

--- wink-compiler-hook-lib/src/main/java/com/immomo/wink/compiler/WinkCompilerHookProcessor.java
+++ wink-compiler-hook-lib/src/main/java/com/immomo/wink/compiler/WinkCompilerHookProcessor.java
@@ -1,6 +1,7 @@
 package com.immomo.wink.compiler;
 
 import com.google.auto.service.AutoService;
+import com.sun.source.util.Trees;
 import com.sun.tools.javac.code.Symbol;
 
 import java.io.File;
@@ -20,6 +21,8 @@ import javax.lang.model.element.TypeElement;
 @SupportedAnnotationTypes({"*"})
 public class WinkCompilerHookProcessor extends AbstractProcessor {
 
+    private Trees trees;
+
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         System.out.println("=======================");
@@ -27,6 +30,59 @@ public class WinkCompilerHookProcessor extends AbstractProcessor {
         System.out.println("=======================");
 
         System.out.println("TypeElement ===>>> : " + set.toString());
+
+        /*
+            出错情况：直接执行脚本
+                ~~~~~~~~~~~~~~~~~~~~~~~~ getClass().getClassLoader(): java.net.URLClassLoader@791900c1
+                ~~~~~~~~~~~~~~~~~~~~~~~~ Trees.class.getClassLoader(): java.net.URLClassLoader@791900c1
+                ~~~~~~~~~~~~~~~~~~~~~~~~ processingEnv.getClass().getClassLoader() 1: java.net.URLClassLoader@63e31ee
+            正常情况：命令行执行
+                ~~~~~~~~~~~~~~~~~~~~~~~~ getClass().getClassLoader(): java.net.URLClassLoader@7edae3a
+                ~~~~~~~~~~~~~~~~~~~~~~~~ Trees.class.getClassLoader(): sun.misc.Launcher$ExtClassLoader@5f2050f6
+                ~~~~~~~~~~~~~~~~~~~~~~~~ processingEnv.getClass().getClassLoader() 1: sun.misc.Launcher$ExtClassLoader@5f2050f6
+
+            This can be validated by running the jvm with -verbose:class:
+            ...
+            [Loaded com.sun.source.util.Trees from file:/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/lib/tools.jar]
+            [Loaded com.sun.source.util.Trees from file:/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/lib/tools.jar]
+            ...
+            You can see [com.sun.source.util.Trees] shows up twice, which confirms our theory.
+         */
+
+        // https://youtrack.jetbrains.com/issue/KT-24540
+        // https://github.com/JetBrains/kotlin/pull/1772 [Kapt] Pass current ClassLoader as parent class loader by default
+        // java.lang.AssertionError: java.lang.ClassCastException: com.sun.tools.javac.api.JavacTrees cannot be cast to com.sun.source.util.Trees
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~ getClass().getClassLoader(): "
+                + getClass().getClassLoader().toString());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~ Trees.class.getClassLoader(): "
+                + Trees.class.getClassLoader().toString());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~ processingEnv.getClass().getClassLoader() 111: "
+                + processingEnv.getClass().getClassLoader().toString());
+
+        // 此处模拟 ButterKnife 注解处理器出现异常时情况
+        /*
+        try {
+            trees = Trees.instance(processingEnv);
+//            System.out.println("Trees ===>>> : 1111111111111");
+        } catch (IllegalArgumentException ignored) {
+            System.out.println("Trees ===>>> : 22222222222222222");
+            System.out.println("Trees ===>>> : " + ignored.toString());
+            try {
+                // Get original ProcessingEnvironment from Gradle-wrapped one or KAPT-wrapped one.
+                for (Field field : processingEnv.getClass().getDeclaredFields()) {
+                    if (field.getName().equals("delegate") || field.getName().equals("processingEnv")) {
+                        field.setAccessible(true);
+                        ProcessingEnvironment javacEnv = (ProcessingEnvironment) field.get(processingEnv);
+                        trees = Trees.instance(javacEnv);
+                        break;
+                    }
+                }
+            } catch (Throwable ignored2) {
+                System.out.println("Trees ===>>> : " + ignored2.toString());
+                System.out.println("Trees ===>>> : 33333333333");
+            }
+        }
+        */
 
         try {
 //            File file = new File("../.idea/wink/annotation/whitelist");
@@ -72,6 +128,8 @@ public class WinkCompilerHookProcessor extends AbstractProcessor {
                         if (symbol == null) {
                             continue;
                         }
+                        // 文件地址
+//                        symbol.sourcefile.getName();
 
                         Field fileField = symbol.sourcefile.getClass().getDeclaredField("file");
                         fileField.setAccessible(true);
