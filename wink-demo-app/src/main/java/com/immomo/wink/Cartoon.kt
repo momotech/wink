be1d5f8f4a0952c9b885f3b9a074ca34c8899581
wangzihang <wang.zihang@imomo.com>
2021-08-23 12:34:02 +0800

    增加第三方库 databinding

---
+++ wink-demo-app/src/main/java/com/immomo/wink/Cartoon.kt
@@ -0,0 +1,5 @@
+package com.immomo.wink
+
+class Cartoon(var name:String = "奇妙冒险" , var series :String = "黄金之风",
+              var leader : String = "乔鲁诺·乔巴纳", var feature : String = "黄金体验") {
+}
---
+++ wink-demo-app/src/main/java/com/immomo/wink/MainActivity4.kt
@@ -0,0 +1,25 @@
+package com.immomo.wink
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.immomo.wink.databinding.ActivityMain4Binding
+import org.greenrobot.eventbus.EventBus
+
+@Route(path = "/com/Activity4444")
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+        val bindingBinding : ActivityMain4Binding = DataBindingUtil.setContentView(this, R.layout.activity_main4)
+        bindingBinding.cartoon = Cartoon()
+    }
+
+    fun doClick(view: View){
+        Toast.makeText(this, "点击测试111", Toast.LENGTH_SHORT).show()
+    }
+}
